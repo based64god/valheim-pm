1dcbca981c02f4e104e2bec8c2b18770ff27cfd1
Emmett Hitz <emmetthitz@gmail.com>
2021-03-17 02:42:25 -0400

    oops

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route("/", methods=['POST'])        # Standard Flask endpoint
 def run_github_webhook():
     parsed = json.loads(request.data)
-    print(parse)
+    print(parsed)
     return 'ok'
 
 
