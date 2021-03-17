5955828e187b1bdc0da35c536ce083fe60bea2e4
Emmett Hitz <emmetthitz@gmail.com>
2021-03-17 01:43:07 -0400

    add main

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import Flask
+#!/usr/bin/env python3
+import flask
+import click
+import os
+
+app = flask.Flask(__name__)
+
+@click.command()
+@click.option('--package-dir', default=os.getcwd(), prompt='system path to package dir', 
+    help='an absolute path to where your valheim packages should be installed')
+
+@app.route('/payload', methods=['POST'])
+def run_github_webhook(package_dir):
+    json = request.json()
+    if json is None:
+        print('failed on request': request)
+        return
+    print('i got json:', json)
+    
