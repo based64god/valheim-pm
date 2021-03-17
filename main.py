6605e05b7f9ea29504276f71bf178eb55698592a
Emmett Hitz <emmetthitz@gmail.com>
2021-03-17 02:37:55 -0400

    remove flask

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route("/", methods=['POST'])        # Standard Flask endpoint
 def run_github_webhook():
     data = json.loads(request.data)
-    print(flask.request.data)
+    print(request.data)
     print(data)
     return
 
