3f04a3af1ce2b9a992db2dbac5cf4b9768b6959d
Emmett Hitz <emmetthitz@gmail.com>
2021-03-17 01:57:28 -0400

    switch to github_webhook

--- main.py
+++ main.py
@@ -2,20 +2,25 @@
 import flask
 import click
 import os
+from github_webhook import Webhook
 
 secret=os.getenv('SECRET')
 
 app = flask.Flask(__name__)
+webhook=Webhook(app)
 
 @click.command()
 @click.option('--package-dir', default=os.getcwd(), prompt='system path to package dir', 
     help='an absolute path to where your valheim packages should be installed')
 
-@app.route('/payload', methods=['POST'])
-def run_github_webhook(package_dir):
-    json = request.json()
-    if json is None:
-        print('failed on request': request)
-        return
-    print('i got json:', json)
-    
+@app.route("/")        # Standard Flask endpoint
+def hello_world():
+    return "Hello, World!"
+
+@webhook.hook()        # Defines a handler for the 'push' event
+def on_push(data):
+    print("Got push with: {0}".format(data))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
